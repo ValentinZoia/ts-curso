58f872a40af7d384f1dd6328a91afcf7a69d8e2a
ValentinZoia <valentinzoia@gmail.com>
2025-02-26 17:53:09 -0300

    add clase de as const

---
+++ ts-sergi/src/components/Gentleman_Curso/as_const_Y_tipado_preciso/index.ts
@@ -0,0 +1,34 @@
+//hagamos un tipo que dependa de una property para el resultado de la función
+
+type Dependant<T extends {property:any}>= T["property"];
+
+type Independant = {
+    property:string;
+}
+
+const dependant:Dependant<Independant> = "1"; // const dependant: string
+
+enum Numbers1 {
+    "NUMBER1" = "number1",
+    "NUMBER2" = "number2",
+}
+
+enum Numbers2 {
+    "NUMBER3" = "number3",
+    
+}
+//juntamos los enums
+const myNumbers = {...Numbers1, ...Numbers2} as const; // al ponerle as const, se convierte en un objeto inmutable
+
+//obtenemos los valores de los enums
+const mixValues = Object.values(myNumbers)
+
+
+type MixNumbers = (typeof mixValues)[number];
+
+type Enums ={
+    [key in MixNumbers]:string;
+}
+
+console.log(myNumbers, "myNumbers");//{ NUMBER1: 'number1', NUMBER2: 'number2', NUMBER3: 'number3' }
+console.log("mixValeus", mixValues);//[ 'number1', 'number2', 'number3' ]
